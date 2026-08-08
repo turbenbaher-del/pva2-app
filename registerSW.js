05fc899091907dc099b55181e1a6d9bf6cc8667e
PVA2 build <pva2@local>
2026-08-08 06:48:51 +0300

    Исправлен базовый путь публикации (/pva2-app/) — приложение не загружалось

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/pva2-app/sw.js', { scope: '/pva2-app/' })})}
